8c35af7d51113bd86f9820173c79ef0a3d17b98e
RahulAnup <rahulanup03@gmail.com>
2023-11-14 14:51:22 +0530

    minor changes

--- src/components/SignupR/SignupR.jsx
+++ src/components/SignupR/SignupR.jsx
@@ -1,42 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignupR.css";
-
 import { Link } from "react-router-dom";
+import axios from "axios"; // Import Axios
 
-function Signup(){
+function Signup() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [type, setType] = useState("recruiter");
 
-    return(
-        <>
-            <form>
-                <div className='box'>
-                    <div className='signup_container'>
-                        <div className='content'>
-                            <div>
-                                <h1>RECRUITER SignUP</h1>
-                            </div>
-                            <div>
-                                <input type="text" placeholder="Username" name="username" />
-                            </div>
-                            <div>
-                                <input type="email" placeholder="Email" name="email" />
-                            </div>
-                            <div>
-                                <input type="password" placeholder="Password" name="password" />
-                            </div>
-                            <div>
-                                <button>SIGN UP</button>
-                                <Link to="/loginR">goto</Link>
-                            </div>
-                            
-                            <div className="linker">
-                                <Link className="linker" to='/loginR'>ALREADY HAVE AN ACCOUNT?</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </>
-    );
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "https://dbms-jgsk.onrender.com/api/user/signup",
+        {
+          username,
+          email,
+          password,
+          type,
+        }
+      );
+      // Handle success - maybe redirect or show a success message
+      console.log("Response:", response.data);
+    } catch (error) {
+      // Handle error - show an error message or do something else
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="box">
+          <div className="signup_container">
+            <div className="content">
+              <div>
+                <h1>RECRUITER SignUP</h1>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div>
+                <input type="submit" value="SIGN UP" />
+                <Link to="/loginR">goto</Link>
+              </div>
+
+              <div className="linker">
+                <Link className="linker" to="/loginR">
+                  ALREADY HAVE AN ACCOUNT?
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default Signup;
