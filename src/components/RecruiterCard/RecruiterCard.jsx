7b1dab9b116d23c3cea25cf6cedbea6b2efcf5e1
RahulAnup <rahulanup03@gmail.com>
2023-11-14 16:02:14 +0530

    delete button

--- src/components/RecruiterCard/RecruiterCard.jsx
+++ src/components/RecruiterCard/RecruiterCard.jsx
@@ -16,6 +16,21 @@ function JobList() {
       });
   }, []);
 
+  const deleteJob = async (jobId) => {
+    try {
+      // Make a DELETE request to your API endpoint with the job ID
+      const response = await axios.delete(`https://dbms-jgsk.onrender.com/api/list-job/delete/${jobId}`); // Replace with your actual DELETE API endpoint
+
+      // If the deletion was successful, update the job list by filtering out the deleted job
+      if (response.status === 200) {
+        setJobData(prevJobData => prevJobData.filter(job => job._id !== jobId));
+        console.log(`Job with ID ${jobId} deleted successfully`);
+      }
+    } catch (error) {
+      console.error('Error deleting job:', error);
+    }
+  };
+
   return (
     <div>
       <h1>Job List</h1>
@@ -26,6 +41,10 @@ function JobList() {
               <h2>{job.title}</h2>
               <p>{job.company} - {job.location}</p>
               <p>{job.description}</p>
+              <div>
+                <button onClick={() => deleteJob(job._id)}>Delete</button>
+                <button>Update</button>
+              </div>
             </li>
           ))}
         </ul>
