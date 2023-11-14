3c82e33e270fa0f7646f5870839a03c6d64e57ee
NzM <nazimfilzer@gmail.com>
2023-11-14 15:32:45 +0530

    Get All jobs Completed

--- src/components/RecruiterCard/RecruiterCard.jsx
+++ src/components/RecruiterCard/RecruiterCard.jsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import "./RecruiterCard.css";
-import {AiOutlineClose} from "react-icons/ai";
-import {GrUpdate} from "react-icons/gr";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { AiOutlineClose } from 'react-icons/ai';
+import { GrUpdate } from 'react-icons/gr';
+import './RecruiterCard.css';
+
 function RecruiterCard() {
+  const [jobData, setJobData] = useState(null);
+
+  useEffect(() => {
+    // Retrieve the JWT token from localStorage
+    const token = localStorage.getItem('token');
+
+    // Make a GET request to your API endpoint with the Authorization header
+    axios.get('https://dbms-jgsk.onrender.com/api/list-job/', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(response => {
+        setJobData(response.data);
+        console.log('Job data:', response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching job data:', error);
+      });
+  }, []);
+
   return (
     <>
+      {jobData && (
         <div className="card__container">
-            <div>
-                <h1>SDE III</h1>
-                <h2>Google</h2>
-                <h3>San Francisco</h3>
-                <h3>random desc</h3>
-                <h4>B-TECH/B.E</h4>
-                <h2>100000$-120000$</h2>
-            </div>
-            <div className="del__update">
-                <button><GrUpdate/></button>
-                <button><AiOutlineClose/></button>
-            </div>
+          <div>
+            <h1>{jobData.title}</h1>
+            <h2>{jobData.company}</h2>
+            <h3>{jobData.location}</h3>
+            <h3>{jobData.description}</h3>
+            <h4>{jobData.education}</h4>
+            <h2>{`${jobData.salaryMin}$ - ${jobData.salaryMax}$`}</h2>
+          </div>
+          <div className="del__update">
+            <button><GrUpdate /></button>
+            <button><AiOutlineClose /></button>
+          </div>
         </div>
+      )}
     </>
-  )
+  );
 }
 
 export default RecruiterCard;
